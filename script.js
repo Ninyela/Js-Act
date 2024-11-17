f3e3e5c2d6b1fdbdbf3b012fd83259cb386ee1fd
Ninyela <ninielasongalia11@gmail.com>
2024-11-17 14:54:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 for (let t = 1; t <= 100; t++){
     if (t % 3 == 0 && t % 5 == 0) {
         console.log("FizzBuzz");
-    } else if (t % 5 == 0){
+    } else if (t % 3 == 0){
         console.log("Fizz");
     } else if (t % 5 == 0){
         console.log("Buzz");
